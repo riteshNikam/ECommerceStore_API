dbae4812aace465fd205a03ffaf06321b7f7ee23
riteshNikam <riteshnikam2804@gmail.com>
2025-10-22 07:55:18 +0530

    Add project files.

---
+++ ECommerceStore/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceStore.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Required]
+        [StringLength(15)]
+        public string Role { get; set; } = "Customer";
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
